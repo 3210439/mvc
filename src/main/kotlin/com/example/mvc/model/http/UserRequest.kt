736d5ca325882c1e9835a1b5f62a1af20fb604ce
kimJongWan <kimdeiz@naver.com>
2022-07-02 17:06:30 +0900

    study spring with kotlin

--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,8 +1,11 @@
 package com.example.mvc.model.http
 
+//@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class UserRequest (
     var name:String?=null,
     var age:Int?=null,
     var email:String?=null,
-    var address:String?=null
-        )
+    var address:String?=null,
+    var phoneNumber:String?=null    // 대부분 JSON은 snake case이다 언더바 사용한다는 의미
+)
+
